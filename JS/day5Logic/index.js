f493954ad22f76944d719e3b2958ff69c7bcca9d
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-06-04 10:56:10 +0530

    day5

---
+++ JS/day5Logic/index.js
@@ -0,0 +1,60 @@
+//Some Logical, Programs procedure programs , swapping Ex : grade , bill , interest , employ salary
+
+let game = {
+  gameName: "volrant",
+  price: "free",
+  space: "24gb",
+}; // object
+
+let a = 15;
+let b = 25;
+
+// let temp = a// 15
+// a = b//25
+// b= temp //15
+
+a = b; // 25
+
+b = a; // 25
+
+// a = a+b
+// b = a -b
+
+// a = a-b
+
+let english = 30;
+
+let math = 40;
+
+let html = 50;
+
+let average = (english + math + html) / 3;
+
+console.log(average);
+
+let pi = 3.14;
+let radius = 5;
+
+let circleOfArea = pi * radius ** 2;
+console.log(circleOfArea);
+
+let parameterOfCircle = 2 * pi * radius;
+console.log(parameterOfCircle);
+
+console.log(12 * 3 + 4 - 12 / 2 + (-2 - 3));
+
+
+console.log( 55>3 || 0 &&55==="55"&& 4>1|| false)// 55>3 || 0  || false
+
+
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+//*
+//** 
+//***
+//****
