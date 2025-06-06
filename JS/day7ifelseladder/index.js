29d00cbebd528a164cc69d6ce64a25521dd9392e
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-06-06 10:40:32 +0530

    ifelseladder

---
+++ JS/day7ifelseladder/index.js
@@ -0,0 +1,108 @@
+// if else ladder Statement Range Program with Logical Operators
+//Range Programs (Grade, Interest, Bill) change password logic
+
+let num1 = 1;
+let num2 = 5;
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+} else if (num1 == num2) {
+  console.log("both are same");
+} else {
+  console.log("num1 is not greater num2");
+}
+
+let light = "red";
+
+if (light == "red") {
+  console.log("stop");
+} else if (light == "yellow") {
+  console.log("slow down ");
+} else if (light == "green") {
+  console.log("go"); // "go"//'go'// `go`
+} else {
+  console.log("invalid color");
+}
+
+let budget = 4000;
+
+if (budget >= 13999) {
+  console.log("Manali");
+} else if (budget >= 11999) {
+  console.log("udaipur");
+} else if (budget >= 9999) {
+  console.log("dwarkadish");
+} else if (budget >= 3999) {
+  console.log("mout tappu");
+} else if (budget >= 3999) {
+  console.log("gandinagar");
+} else if (budget >= 999) {
+  console.log("kakariay lake");
+} else {
+  console.log("ghar betho");
+}
+
+let grade = 40;
+
+if (grade >= 90) {
+  console.log("A+");
+} else if (grade >= 70) {
+  console.log("B+");
+} else if (grade >= 60) {
+  console.log("c+");
+} else if (grade >= 50) {
+  console.log("c");
+} else if (grade >= 30) {
+  console.log("D");
+} else {
+  console.log("fail");
+}
+
+let value = 1000;
+
+if (value > 2000 && value < 6000) {// between 
+  console.log("a");
+} else if (value > 999 && value < 10000) {
+  console.log("b");
+}
+
+
+
+// let 
+// const
+// var
+
+// let and const 
+
+
+// global scope
+
+// block scope 
+
+// function scope
+
+
+let game = "volrant"// let keywrod help to reassigned value in same variable name// block scope k ander 
+
+game = "pubg"
+
+game = "freefire"
+
+{
+let game = "csgo"
+  console.log(game)
+}
+
+ console.log(game)
+
+
+
+
+ const state= "ahemdabad"/// const means your values always constant  you cannot reassigned the value 
+
+//  state = "delhi"
+
+ console.log(state)
+
+
+ // var  // global scope  or function scope 
