ddf2ade45a98f0d396d67604af2a9f0b7b079257
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-06-09 10:00:53 +0530

    loop

---
+++ JS/day8whileLoop/index.js
@@ -0,0 +1,46 @@
+// While loop
+// Do while loop
+
+//loop the things were continue
+
+// while (condition) {
+
+///value increment
+
+// } //block
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+
+let start = 1;
+
+// intialization
+// conditon=> block k ander wala code execute hoga
+//
+// increment
+
+// while (start <= 1000) {
+//   //1001<=1000 // when my condtion is false it will comeout from while loop
+
+//   console.log(start); //1//2 to999 /1000
+
+//   start++; //1001
+// }
+
+// intialization
+// code will be  execute  one time
+// increament
+// condtion check hogi
+
+let i = 1;// intialization 
+
+// execute 
+do {
+  console.log("hello",i);//hello 1//hello 2//hello 3//hello 4//hello 5
+  i++//6
+
+} while (i<=5);// false/6<=5//false
