e22bd13fd846bc79ff9152dcbd8eb3a72f273725
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-06-17 10:07:03 +0530

    revision

---
+++ JS/day10function/index.js
@@ -0,0 +1,59 @@
+// // function is a block of code
+
+// // console.log(1);
+// // console.log(2);
+// // console.log(3);
+
+// //3
+// //1
+// //2
+
+// // normal function
+// print3();
+// print1();
+// print2();
+
+// function print1() {
+//   console.log(1);
+// }
+
+// function print2() {
+//   console.log(2);
+// }
+
+// function print3() {
+//   console.log(3);
+// }
+
+// let a = 12; // globally
+// let b = 44;
+
+// function sum(c, d) {
+//   // parameters// num1,num2 // i can give any to parameters
+
+//   return c + d;
+// }
+
+// //arguments// 27
+// console.log(sum(22, 5));
+// console.log(sum(a, b));
+
+// let result = sum(1, 2);
+
+// console.log(result);
+
+// normal function
+
+function show(a, b, c) { // paramters
+
+
+  return a+b+c;//6
+}
+
+let result = show(12, 15, 99); // arguments
+
+console.log(result)
+
+
+
+console.log(show(1,2,3))
