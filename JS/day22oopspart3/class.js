c943f6d8a4b6d18f78c4fd5354bf4bd328bdb859
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-07-07 17:47:57 +0530

    oops4

--- JS/day22oopspart3/class.js
+++ JS/day22oopspart3/class.js
@@ -1,21 +1,29 @@
 class Animal {
+  #petName // private variable
   constructor(petName, color, breed) {
-    this.petName = petName;
+    
+    this.#petName = petName;
+    // this.#petName = "jorden" we can  only update private field in class not outside 
     this.color = color;
     this.breed = breed;
   }
   show( sound) {// parameters
-    console.log(`${this.petName}  ${this.color} ${sound}`);
+    console.log(`${this.#petName}  ${this.color} ${sound}`);
   }
 }
 
 class Dog extends Animal {
   constructor(petName, color, breed) {
     super(petName, color, breed);
+  
   }
+  // show(){// overriding// polymorphism
+  //   console.log("hello dog")
+  // }
 }
 
 let boji = new Dog("boji", "blue", "germanSheperd");
+// boji.#petName = "jorden" is not working because we create private variable with #petName
 console.log(boji);
 
 
---
+++ JS/revision/index.js
@@ -0,0 +1,10 @@
+let array = [12,4,13,5,78,43,78]
+for(let i =0; i <array.length; i++){
+  if(array[i] %2 == 1){
+    console.log(array[i]);
+  }
+}
+
+for(let i =array.length-1; i>=0; i-- ){
+  console.log(array[i]);
+}
