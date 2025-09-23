4abbd73ba99de94539a1969bc3f4dc3961880f32
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-23 18:38:56 +0530

    tailwidn

---
+++ REACT/day12tailwindcss/react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="flex  bg-amber-300 justify-between items-center">
+      <p className="text-blue-900 font-[900] text-[45px]" >hello world </p>
+      <p>Game</p>
+    </div>
+  );
+};
+
+export default App;
