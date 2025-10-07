dd5ef2937d30717c9672e4a2b636591865b49368
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-10-07 18:30:42 +0530

    todoliist

--- REACT/day15todolist/app/src/components/Todolist.jsx
+++ REACT/day15todolist/app/src/components/Todolist.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 
 const Todolist = () => {
   const [inputText, setInputText] = useState("");
+  const [boolean, setBoolean] = useState(false);
+  const [editIndex, setEditIndex] = useState("");
   const [todolist, setTodolist] = useState(
     JSON.parse(localStorage.getItem("todoData")) || []
   );
   function handleAdd() {
     // console.log(inputText);
-    if(!inputText.trim()){
-      alert("first you have to field input")
-      return
+    if (!inputText.trim()) {
+      alert("first you have to field input");
+      return;
     }
 
     setTodolist([inputText, ...todolist]);
@@ -18,6 +20,29 @@ const Todolist = () => {
     setInputText("");
   }
   // console.log(todolist)
+  function handleDelete(i) {
+    let filterData = todolist.filter((element, index) => index != i);
+    setTodolist(filterData);
+  }
+  function handleEdit(index) {
+    setBoolean(true);
+    setEditIndex(index);
+    setInputText(todolist[index]);
+  }
+
+  function handleUpdate() {
+    if (!inputText.trim()) {
+      alert("first you have to feild the task");
+      return;
+    }
+
+    let updateData = todolist.map((element, i) =>
+      i == editIndex ? inputText : element
+    );
+    setTodolist(updateData);
+    setBoolean(false)
+    setInputText("")
+  }
 
   useEffect(() => {
     localStorage.setItem("todoData", JSON.stringify(todolist));
@@ -32,9 +57,31 @@ const Todolist = () => {
           setInputText(e.target.value);
         }}
       />
-      <button onClick={handleAdd}>Add Task</button>
+      {boolean ? (
+        <button onClick={handleUpdate}>Update</button>
+      ) : (
+        <button onClick={handleAdd}>Add Task</button>
+      )}
       {todolist.map((element, index) => {
-        return <p key={index}>{element}</p>;
+        return (
+          <div>
+            <p key={index}>{element}</p>
+            <button
+              onClick={() => {
+                handleDelete(index);
+              }}
+            >
+              Delete
+            </button>
+            <button
+              onClick={() => {
+                handleEdit(index);
+              }}
+            >
+              Edit
+            </button>
+          </div>
+        );
       })}
     </div>
   );
